ba0ee0c82975671d352bde3f65883a2cf9966fb6
HarshRaj3050 <harshraj3060@gmail.com>
2025-10-26 14:24:07 +0530

    deletion at beginning - linked list

--- 04_LinkedList/yt_100.cpp
+++ 04_LinkedList/yt_100.cpp
@@ -2,12 +2,10 @@
 #include <vector>
 using namespace std;
 
-struct Node
-{
+struct Node{
     int data;
-    Node *next;
-    Node(int value)
-    {
+    Node* next;
+    Node(int value) {
         data = value;
         next = NULL;
     }
@@ -16,34 +14,42 @@ struct Node
 Node* head = NULL;
 Node* tail = head;
 
-void insertAtEnd(int value)
-{
+void insertAtBeginning(int value){
     Node* newNode = new Node(value);
     if(head == NULL){
         head = newNode;
         tail = head;
-    } else {
-        tail->next = newNode;
-        tail = tail->next;
+        return;
     }
+    newNode->next = head;
+    head = newNode;
+}
 
+void deletionAtBeginning(){
+    if(head == NULL){
+        return;
+    }
+    Node* temp = head;
+    head= head->next;
+    delete temp;
 }
 
 void display(){
     Node* temp = head;
-    while(temp!=NULL){
+    while(temp != NULL){
         cout<<temp->data<<((temp->next == NULL)?"": " -> ");
         temp = temp->next;
     }
 }
 
-int main()
-{
-    insertAtEnd(1);
-    insertAtEnd(2);
-    insertAtEnd(3);
-    insertAtEnd(4);
-    insertAtEnd(5);
-
+int main(){
+    insertAtBeginning(2);
+    insertAtBeginning(4);
+    insertAtBeginning(6);
+    insertAtBeginning(8);
+    cout<<"----Before Deletion----"<<endl;
+    display();
+    deletionAtBeginning();
+    cout<<"\n----After Deletion----"<<endl;
     display();
 }

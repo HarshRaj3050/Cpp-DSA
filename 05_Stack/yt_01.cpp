7426b9799f1596c7eb41f00eb89be5ceae77c560
HarshRaj3050 <harshraj3060@gmail.com>
2025-10-30 23:52:26 +0530

    stack push, pop, peek

---
+++ 05_Stack/yt_01.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack
+{ 
+    int *arr;
+    int size;
+    int top;
+
+    public:
+    Stack(int s){
+        size = s;
+        top = -1;
+        arr = new int[s]; 
+    }
+
+    void push(int value){
+        if(top == size-1){
+            cout<<"stack is overflow!!"<<endl;
+            return;
+        }
+        top++;
+        arr[top] = value;
+        cout<<"pushed "<<value<<" into the stack"<<endl;;
+    }
+
+    void pop(){
+        if(top == -1){
+            cout<<"stack is underflow!!"<<endl;
+            return;
+        }
+        cout<<"poped "<<arr[top]<<" from the stack"<<endl;
+        top--;
+    }
+
+    int peek(){
+        if(top == -1){
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+
+    int isSize(){
+        return top+1;
+    }
+};
+
+main()
+{
+    Stack st(5);
+
+    int n=0;
+    switch(n != 5){
+        case 1:
+            int num;
+            cout<<"Enter the Number: ";
+            cin>>num;
+            st.push(num);
+            break;
+        case 2:
+            st.pop();
+            break;
+        case 3:
+            int value = st.peek();
+            if (st.isSize() != 0)
+            {
+                cout << value << endl;
+            }
+            break;
+    }
+}

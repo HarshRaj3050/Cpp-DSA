947dd411a8e81fc26759889151968fbc7292f424
HarshRaj3050 <harshraj3060@gmail.com>
2025-10-30 23:00:38 +0530

    staring stack

--- 04_LinkedList/yt_100.cpp
+++ 04_LinkedList/yt_100.cpp
@@ -18,32 +18,39 @@ struct Node
 Node *head = NULL;
 Node *tail = head;
 
-void insertAtEnd(int value) {
-    Node* newNode = new Node(value);
-    if(head == NULL){
+void insertAtEnd(int value)
+{
+    Node *newNode = new Node(value);
+    if (head == NULL)
+    {
         head = newNode;
         tail = head;
-    } else {
+    }
+    else
+    {
         tail->next = newNode;
         tail = tail->next;
     }
 }
 
-Node* deletionAtPosition(Node* curr,int position) {
-    if(position == 1){
-        Node* temp = curr;
-        curr = curr->next;
-        delete temp;
-        return curr;
+Node *deletionAtPosition(Node *curr, int position)
+{
+    if (position == 1)
+    {
+        Node *temp = curr->next;
+        delete curr;
+        return temp;
     }
-    curr->next = deletionAtPosition(curr->next, position-1);
+    curr->next = deletionAtPosition(curr->next, position - 1);
     return curr;
 }
 
-void display(){
-    Node* temp = head;
-    while(temp != NULL){
-        cout<<temp->data<<((temp->next == NULL)? "": " -> ");
+void display()
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << ((temp->next == NULL) ? "" : " -> ");
         temp = temp->next;
     }
 }
@@ -51,7 +58,8 @@ void display(){
 int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5};
-    for(int i=0; i<arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++)
+    {
         insertAtEnd(arr[i]);
     }
     head = deletionAtPosition(head, 4);

4f878f41b75ee39743fa1e9e576cbdd963b7126e
HarshRaj3050 <harshraj3060@gmail.com>
2025-11-13 02:43:18 +0530

    circular queue practice

--- 06_Queue/yt_100.cpp
+++ 06_Queue/yt_100.cpp
@@ -2,86 +2,75 @@
 using namespace std;
 
 class Queue{
-    int size;
-    int front = -1, rear = -1;
     public:
-    int *list;
+    int size = 5;
+    int *arr = new int[size];
+    int front = -1, rear = -1;
+    
 
-    Queue(int s){
-        size = s;
-        list = new int[s];
-    }
 
     bool isEmpty(){
-        return front==-1;
+        if(front == -1){
+            return true;
+        }
+        return false;
     }
 
     bool isFull(){
         return (rear+1)%size == front;
     }
 
-    void push(int value){
+    void insert(int value){
         if(isEmpty()){
+            arr[0] = value;
             front = rear = 0;
-            list[0] = value;
-        } else {
-            if(isFull()){
-                cout<<"Queue is Full!"<<endl;
-            } else {
-                list[rear] = value;
-                rear = (rear+1)%size;
-            }
+            return;
         }
+
+        if(isFull()){
+            return;
+        }
+
+        rear = (rear+1)%size;
+        arr[rear] = value;
     }
 
-    void pop(){
+    void remove(){
         if(isEmpty()){
-            cout<<"Queue is Empty!!"<<endl;
+            cout<<"Queue is Empty!!";
+        } else if(front == rear){
+            front = rear = -1;
         } else {
             front = (front+1)%size;
         }
     }
 
-    void start(){
-        if(isEmpty()){
-            cout<<"Queue is Empty!!"<<endl;
-        } else {
-            cout<<list[front];
-        }
-    }
-
     void display(){
-        if (isEmpty())
-        {
-            cout << "Queue is Empty!!" << endl;
-            return;
+        if(isEmpty()){
+            cout<<"Queue is Empty!!";
+        } else {
+            int temp = front;
+            while(true){
+                cout<<arr[temp]<<" ";
+                if ((temp % size) == rear) break;
+                temp = (temp+1)%size;
+
+                
+            }
         }
-        int i = front;
-        while (true)
-        {
-            cout << list[i] << " ";
-            if ((i % size) == rear)
-                break;
-            i = (i + 1) % size;
-        }
-        cout << endl;
     }
-
-
-
 };
 
 int main(){
-    Queue q(5); // Max storable items = 4 (size-1)
-    q.push(10);
-    q.push(20);
-    q.push(30);
+    Queue q;
+    q.insert(5);
+    q.insert(2);
+    q.insert(3);
+    q.insert(7);
+    q.insert(8);
+    q.remove();
+    q.remove();
+    q.insert(1);
+    q.insert(10);
     q.display();
-    q.pop();
-    q.display();
-    q.push(40);
-    q.push(50);
-    q.push(60);
-    q.display();
-    q.push(70);
 }
